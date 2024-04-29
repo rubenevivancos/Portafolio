d9bed54bef2a378074e5b093ff1e5dafaf45e51b
rubenevivancos <rubenevivancos@hotmail.com>
2024-04-28 21:08:33 -0500

    Terminando pagina Sobre mi

--- client/src/Components/AboutMe/aboutMe.js
+++ client/src/Components/AboutMe/aboutMe.js
@@ -17,7 +17,14 @@ export default function Home() {
                                 Sobre mí
                             </h1>
                         </div>
-                        <p className="text-white">El resto del contenido va aquí y se alinea a la izquierda.</p>
+                        <div className="d-flex justify-content-center text-white px-5 py-5 text-justify">
+                            <p className="w-75">
+                                Mi nombre es Rubén Vivanco Salazar, soy un Desarrollador Full Stack con sede en Lima-Perú.
+                                Tengo más de 5 años de experiencia como desarrollador, principalmente con las tecnologías Java y Oracle.
+                                Adquirí nuevos conocimientos en el bootcamp SoyHenry y ahora quiero empezar una nueva etapa, nuevos retos con éstas 
+                                nuevas tecnologías trabajando en forma remota.
+                            </p>
+                        </div>
                     </Col>
                 </Row>
             </Container>
