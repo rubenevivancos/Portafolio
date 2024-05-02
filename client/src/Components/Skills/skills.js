290312d09ed6ad6ed0b0c04c5eb1fee8537c82f2
rubenevivancos <rubenevivancos@hotmail.com>
2024-05-01 20:38:20 -0500

    Mejorando diseño

--- client/src/Components/Skills/skills.js
+++ client/src/Components/Skills/skills.js
@@ -31,31 +31,31 @@ export default function Home() {
                             <div className="w-75">
                                 <Row>
                                     <Col className="text-white">
-                                        <p className="mb-2 d-flex justify-content-center align-items-center">
-                                            <Image src={reactBootstrap} className="me-2" width="16" height="16"/>React-Bootstrap <span className="text-danger"><BsCheck /></span>
+                                        <p className="mb-4 d-flex justify-content-start align-items-center">
+                                            <Image src={reactBootstrap} className="me-2" width="48" height="48"/>React-Bootstrap <span className="text-danger"><BsCheck /></span>
                                         </p>
-                                        <p className="mb-2 d-flex justify-content-center  align-items-center">
-                                            <Image src={bootstrap} className="me-2" width="16" height="16"/>Bootstrap <span className="text-danger"><BsCheck /></span>
+                                        <p className="mb-4 d-flex justify-content-start  align-items-center">
+                                            <Image src={bootstrap} className="me-2" width="48" height="48"/>Bootstrap <span className="text-danger"><BsCheck /></span>
                                         </p>
-                                        <p className="mb-2 d-flex justify-content-center align-items-center">
-                                            <Image src={react} className="me-2" width="16" height="16"/>React <span className="text-danger"><BsCheck /></span>
+                                        <p className="mb-4 d-flex justify-content-start align-items-center">
+                                            <Image src={react} className="me-2" width="48" height="48"/>React <span className="text-danger"><BsCheck /></span>
                                         </p>
-                                        <p className="mb-2 d-flex justify-content-center align-items-center">
-                                            <Image src={reduxToolkit} className="me-2" width="16" height="16"/>Redux-Toolkit <span className="text-danger"><BsCheck /></span>
+                                        <p className="mb-4 d-flex justify-content-start align-items-center">
+                                            <Image src={reduxToolkit} className="me-2" width="48" height="48"/>Redux-Toolkit <span className="text-danger"><BsCheck /></span>
                                         </p>
                                     </Col>
                                     <Col className="text-white">
-                                        <p className="mb-2 d-flex justify-content-center align-items-center">
-                                            <Image src={node} className="me-2" width="16" height="16"/>Node <span className="text-danger"><BsCheck /></span>
+                                        <p className="mb-4 d-flex justify-content-start align-items-center">
+                                            <Image src={node} className="me-2" width="48" height="48"/>Node <span className="text-danger"><BsCheck /></span>
                                         </p>
-                                        <p className="mb-2 d-flex justify-content-center align-items-center">
-                                            <Image src={express} className="me-2" width="16" height="16"/>Express <span className="text-danger"><BsCheck /></span>
+                                        <p className="mb-4 d-flex justify-content-start align-items-center">
+                                            <Image src={express} className="me-2" width="48" height="48"/>Express <span className="text-danger"><BsCheck /></span>
                                         </p>
-                                        <p className="mb-2 d-flex justify-content-center align-items-center">
-                                            <Image src={sequelize} className="me-2" width="16" height="16"/>Sequelize <span className="text-danger"><BsCheck /></span>
+                                        <p className="mb-4 d-flex justify-content-start align-items-center">
+                                            <Image src={sequelize} className="me-2" width="48" height="48"/>Sequelize <span className="text-danger"><BsCheck /></span>
                                         </p>
-                                        <p className="mb-2 d-flex justify-content-center align-items-center">
-                                            <Image src={postgreSQL} className="me-2" width="16" height="16"/>PostgreSQL <span className="text-danger"><BsCheck /></span>
+                                        <p className="mb-4 d-flex justify-content-start align-items-center">
+                                            <Image src={postgreSQL} className="me-2" width="48" height="48"/>PostgreSQL <span className="text-danger"><BsCheck /></span>
                                         </p>
                                     </Col>
                                 </Row>
